3c99f45dec0cf6efbc5472eafb12a0ec00b0262a
CdFerneding <bencf@stud.ntnu.no>
2023-09-27 11:53:32 +0200

    berry bush get's his own scene and the scene will be placed into the tilemap instead of the bushes (get by index)

--- Scripts/building/building.gd
+++ Scripts/building/building.gd
@@ -11,20 +11,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-#func _on_area_entered(body):
-#	if(body.is_in_group("Unit")):
-#		print(body.position)
-#		for i in range(body.previous_positions.size() -1, -1,-1):
-#			var pos = body.previous_positions[i]
-#			body.position = pos
-#			print(body.position)
-		
-
-
-#unc _on_area_exited(body):
-#	if(body.is_in_group("Unit")):
-#		print(body)
-
-	
