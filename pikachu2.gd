08f81805e747bc5733b94ea0ac1c28008ba53f33
TxRuX <maharneu@gmail.com>
2023-09-27 10:28:37 +0200

    New Pickachu

---
+++ pikachu2.gd
@@ -0,0 +1,116 @@
+extends CharacterBody2D
+
+
+signal hit
+
+@export var speed = 200 # How fast the player will move (pixels/sec).
+@export var tilemap: TileMap
+var limit_right
+var limit_bottom
+var previous_direction
+var previous_positions = []
+var currentDir
+
+var vector_minimum_top_left
+var vector_maximum_bottom_right
+
+
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+
+
+func _ready():
+	previous_direction = "down"
+	$AnimatedSprite2D.animation = "idle_down"
+	hide()
+	update_limit()
+	
+func update_limit():
+	var shape = $CollisionShape2D.shape.get_rect().size
+	
+	var mapRect = tilemap.get_used_rect()
+	var tileSize = tilemap.cell_quadrant_size
+	var worldSizeInPixels = mapRect.size * tileSize
+	limit_right = worldSizeInPixels.x
+	limit_bottom = worldSizeInPixels.y
+	
+	vector_minimum_top_left = Vector2(shape.x/2,shape.y)
+	
+	vector_maximum_bottom_right = Vector2(worldSizeInPixels.x-shape.x/2, worldSizeInPixels.y-shape.y/2)
+
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if  Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down") :
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	
+	if(previous_positions.size() < 4):
+		previous_positions.append(position)
+	else:
+		previous_positions.pop_front()
+		previous_positions.append(position)
+	position += velocity * delta
+
+	
+	$AnimatedSprite2D.play()
+	
+	if velocity == Vector2.ZERO:
+		$AnimatedSprite2D.animation = "idle_"+previous_direction
+	else:
+		var current_animation = ""
+		
+		if velocity.y < 0:
+			current_animation+="up_"
+		elif velocity.y > 0:
+			current_animation+="down_"
+			
+		if velocity.x < 0:
+			current_animation+="left_"
+		elif velocity.x > 0:
+			current_animation += "right_"
+		
+		previous_direction = current_animation.left(current_animation.length() - 1)
+		current_animation = "walking_"+previous_direction
+		$AnimatedSprite2D.animation = current_animation
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
