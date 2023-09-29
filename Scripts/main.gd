dd12b0e83be629b1518e5069b86110f191bb1965
TxRuX <maharneu@gmail.com>
2023-09-29 16:03:49 +0200

    working on pikatchu when target arrived but walking animation is still not properly working

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -6,8 +6,11 @@ var score
 # storing the selected pikachu
 var selected_pikachu = []
 var selected_destination
-var hover = false
 var pik_hover = false
+
+
+#deprecated attributes
+var hover = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,32 +19,27 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	pikatchu_scale_on_hover()
+		
 	
 func new_game():
 	score = 0
 	$Pikachu.start($StartPosition.position)
 
-func _on_pikachu_mouse_entered():
-	hover = true
-
-func _on_pikachu_mouse_exited():
-	hover = false
 
 func _unhandled_input(event):
 	if selected_pikachu.size() != 0:
 		if Input.is_action_pressed("right_click"):
-			#selected_destination = get_viewport().get_mouse_position() 
-			#print(selected_pikachu[0], " from: ", selected_pikachu[0].position , " to: ", selected_destination)
 			# a star algorithm here
 			for p in selected_pikachu:
 				p.make_path()
 			# end of a star algorithm, pikachu arrived
 			selected_pikachu = []
 	if pik_hover and Input.is_action_pressed("left_click"):
-		#print("hello world")
 		selected_pikachu.append($Pikachu_2)
-		hover = false
+		pik_hover = false
+	elif Input.is_action_pressed("left_click"):
+		pik_hover = false
 
 
 
@@ -51,3 +49,19 @@ func _on_pikachu_2_mouse_exited():
 
 func _on_pikachu_2_mouse_entered():
 	pik_hover = true
+	
+func pikatchu_scale_on_hover() -> void:
+	if pik_hover:
+		$Pikachu_2.scale.x = 1.1
+		$Pikachu_2.scale.y = 1.1
+	else:
+		$Pikachu_2.scale.x = 1.0
+		$Pikachu_2.scale.y = 1.0
+
+#deprecated functions
+
+func _on_pikachu_mouse_entered():
+	hover = true
+
+func _on_pikachu_mouse_exited():
+	hover = false
--- pikachu2.gd
+++ pikachu2.gd
@@ -1,122 +1,66 @@
 extends CharacterBody2D
 
-const speed = 200
+#speed of moving Pikatchu
+const speed = 50
 
-signal pikachu_2(pik)
+#used to detect when path is reached
+var target : Vector2
 
-var player : Node2D
+#implements the pathfinding algorithm
 @onready var nav_agent:= $NavigationAgent2D as NavigationAgent2D
 
 func _process(delta:float):
-	var mouse_position = get_viewport().get_mouse_position()
+	pass
 	
-	#make_path()
-	
-
 func _physics_process(_delta: float) -> void:
 	
+	var prev_vel = velocity
+	if self.position == target:
+		return
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	#print(nav_agent.get_next_path_position(), " : ", get_global_mouse_position()) 
 	velocity = dir * speed
-	move_and_slide()
 	
+	apply_corresponding_animation(prev_vel)
+	
+	move_and_slide()
 	
 func make_path() -> void:
 	nav_agent.target_position = get_global_mouse_position()
+	target = nav_agent.target_position
+	
+#toDo
+#not sufficient yet
+func apply_corresponding_animation(prev):
+	var previous_direction
+	var current_animation = ""
+	
+	#if prev.x == velocity.x and prev.y == velocity.y:
+	#	current_animation += "down_"
+		
+	if prev.y < velocity.y - 0.4:
+		current_animation+="down_"
+	elif prev.y > velocity.y + 0.4:
+		current_animation+="up_"
+		
+	if prev.x > velocity.x + 0.4:
+		current_animation+="left_"
+	elif prev.x < velocity.x - 0.4:
+		current_animation += "right_"
+	
+	previous_direction = current_animation.left(current_animation.length() - 1)
+	current_animation = "walk_"+previous_direction
+	print(current_animation)
+	$AnimatedSprite2D.animation = current_animation
 	
 func _input(event):
-	if event.is_action("left_click"):
-		print("Pikachu clicked")
-		emit_signal("pikachu_2", self)
+	pass
+	
+	#this was a try to start pathfinding algorithem from Pikatchu and not from the main scene
+#	if event.is_action("left_click"):
+#		print("Pikachu clicked")
+		
 		
 		
 		
    
-
-
-
-
-
-#
-#signal hit
-#
-#@export var speed = 200 # How fast the player will move (pixels/sec).
-#@export var tilemap: TileMap
-#var limit_right
-#var limit_bottom
-#var previous_direction
-#var previous_positions = []
-#var currentDir
-#
-#var vector_minimum_top_left
-#var vector_maximum_bottom_right
-#
-#
-#func _ready():
-#	previous_direction = "down"
-#	#$AnimatedSprite2D.animation = "idle_down"
-#	hide()
-#	update_limit()
-#
-#func update_limit():
-#	var shape = $CollisionShape2D.shape.get_rect().size
-#
-#	var mapRect = tilemap.get_used_rect()
-#	var tileSize = tilemap.cell_quadrant_size
-#	var worldSizeInPixels = mapRect.size * tileSize
-#	limit_right = worldSizeInPixels.x
-#	limit_bottom = worldSizeInPixels.y
-#
-#	vector_minimum_top_left = Vector2(shape.x/2,shape.y)
-#
-#	vector_maximum_bottom_right = Vector2(worldSizeInPixels.x-shape.x/2, worldSizeInPixels.y-shape.y/2)
-#
-#func _process(delta):
-#	var velocity = Vector2.ZERO # The player's movement vector.
-#	if  Input.is_action_pressed("move_right"):
-#		velocity.x += 1
-#	if Input.is_action_pressed("move_left"):
-#		velocity.x -= 1
-#	if Input.is_action_pressed("move_down") :
-#		velocity.y += 1
-#	if Input.is_action_pressed("move_up"):
-#		velocity.y -= 1
-#
-#
-#	if velocity.length() > 0:
-#		velocity = velocity.normalized() * speed
-#		$AnimatedSprite2D.play()
-#
-#	if(previous_positions.size() < 4):
-#		previous_positions.append(position)
-#	else:
-#		previous_positions.pop_front()
-#		previous_positions.append(position)
-#	position += velocity * delta
-#
-#
-#	$AnimatedSprite2D.play()
-#
-#	if velocity == Vector2.ZERO:
-#		$AnimatedSprite2D.animation = "idle_"+previous_direction
-#	else:
-#		var current_animation = ""
-#
-#		if velocity.y < 0:
-#			current_animation+="up_"
-#		elif velocity.y > 0:
-#			current_animation+="down_"
-#
-#		if velocity.x < 0:
-#			current_animation+="left_"
-#		elif velocity.x > 0:
-#			current_animation += "right_"
-#
-#		previous_direction = current_animation.left(current_animation.length() - 1)
-#		current_animation = "walking_"+previous_direction
-#		$AnimatedSprite2D.animation = current_animation
-#
-#func start(pos):
-#	position = pos
-#	show()
-#	$CollisionShape2D.disabled = false
-
