2e47c533c75161f5e6472b506ca62f99236861cd
justin <just.frauboese@gmial.com>
2023-10-19 13:25:15 +0200

    You can now create different buildings when you have 20 wood, when you have 10 food you can create a new unit when you click on a building, added some free sounds I found on the internet, reworked the tileBuilder, organized the input function in main into different function for hopefully better structure. Like always I hope I didn't break anything.

--- Scripts/Data/Game.gd
+++ Scripts/Data/Game.gd
@@ -6,7 +6,12 @@ var Food = 0
 var Wood = 0
 var Stone = 0
 var GameMode = "play"
+var selectedBuilding = "PokeCenter"
+var buildCounter = 0
 var Selected = 0
 
+var buildingCost = 16
+var militaryCost = 10
+var pikachuCost = 8
 var Player_name : String
 
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,8 +1,9 @@
 extends Node
 
-@onready var pikachuBuilder = preload("res://Scripts/Builder/PikachuBuilder.gd")
+@onready var unitBuilder = preload("res://Scripts/Builder/UnitBuilder.gd")
 @onready var pikachu = preload("res://Scenes/pikachu.tscn")
 @export var mob_scene: PackedScene
+@export var tileMap: TileMap
 # @Justin "UILabel is now as a "GameMode" Label in the HUD. 
 # The controlling var is in the Gloabl File Game.gd as GameMode
 # you can delte this
@@ -14,9 +15,7 @@ var selected_pikachus = []
 # all pikachus in the group "pikachus"
 var pikachus = []
 
-# for now gamemode 0 is general mode and 1 is buildmode
-var gamemode = "play"
-
+var buildings = []
 #cursor
 var default_cursor = preload("res://Assets/Sprites/Cursor/test_cursor_2.png")
 #cursor 1 looks horrible - do not use
@@ -32,6 +31,7 @@ func _ready():
 	#pikachuBuilder._build_pikachu(self,self.find_child("TileMap"),start_position)
 	
 	get_pikachus()
+	get_buildings()
 		
 
 func get_pikachus():
@@ -39,9 +39,14 @@ func get_pikachus():
 	pikachus = get_tree().get_nodes_in_group("pikachus")
 
 
+func get_buildings():
+	buildings = []
+	buildings = get_tree().get_nodes_in_group("buildings")
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	_change_gamemode()
+	pass
 	
 	
 func new_game():
@@ -49,9 +54,70 @@ func new_game():
 	$Music.play()
 	# $Pikachu.start($StartPosition.position)
 
-
+'
+first checks if button pressed for a change in gamemode
+and then depending on what is the current gamemode goes to
+the correlating _handle_input function
+'
 func _input(_event):
-#	_add_new_pikachu(event)
+	_change_gamemode()
+	if Game.GameMode == "play":
+		_handle_play_input(_event)
+		
+	elif Game.GameMode == "build" && Game.buildCounter < 3:
+		_handle_building_input(_event)
+		
+	elif Game.GameMode == "place":
+		_handle_place_input(_event)
+
+
+# connected through funciton in pikachu._ready()
+func _on_pikachu_clicked(object: CharacterBody2D):
+	selected_pikachus.append(object)
+	
+'
+different input functions get called depending on what 
+the current gamemode is
+
+building is for when you want to build a building
+you can select different types of buildings and build them
+'
+func _handle_building_input(event):
+
+	if Input.is_action_just_pressed("1"):
+		Game.selectedBuilding = "PokeCenter"
+	elif Input.is_action_just_pressed("2"):
+		Game.selectedBuilding = "FireBuilding"
+	elif Input.is_action_just_pressed("3"):
+		Game.selectedBuilding = "PlantBuilding"
+	elif Input.is_action_just_pressed("4"):
+		Game.selectedBuilding = "WaterBuilding"
+			
+	elif Input.is_action_just_pressed("left_click"):
+		var y_position = floor(tileMap.get_global_mouse_position().y/16)
+		var x_position = floor(tileMap.get_global_mouse_position().x/16)
+		var tile_position = Vector2(x_position,y_position)
+		var selected = Game.selectedBuilding
+		match selected:
+			"PokeCenter":
+				tileMap._place_building(tile_position,4,3,2)
+			"FireBuilding":
+				tileMap._place_building(tile_position,4,4,3)
+			"PlantBuilding":
+				tileMap._place_building(tile_position,4,4,4)
+			"WaterBuilding":
+				tileMap._place_building(tile_position,4,4,5)
+
+
+'
+play_input handles the main gamemode, where you can select and deselect
+units and move them around.
+
+found a small bug if it is not fixed yet where you can select the same
+pikachu multiple times and increases selected count
+'
+func _handle_play_input(event):
+	#_add_new_pikachu(event)
 	var no_pikachus_selected = true
 	# this check needs to check for "release". Otherwise when placing new pikachus they get instantly selected
 	if Input.is_action_just_released("left_click"):
@@ -63,11 +129,21 @@ func _input(_event):
 		if no_pikachus_selected:
 			selected_pikachus = []
 			Game.Selected = selected_pikachus.size()
+			
 	if Input.is_action_just_pressed("right_click"):
 		for p in selected_pikachus:
 			p.make_path()
 
+'
+only for debugging to place units manually
+'
+func _handle_place_input(event):
+	if Input.is_action_just_pressed("left_click"):
+		unitBuilder._build_unit(self, pikachu,tileMap.get_global_mouse_position(),0)
 
+'
+change gamemode depending on different button presses
+'
 func _change_gamemode():
 	if Input.is_action_pressed("B"):
 		Game.GameMode = "build"
@@ -75,8 +151,3 @@ func _change_gamemode():
 		Game.GameMode = "play"
 	if Input.is_action_pressed("P"):
 		Game.GameMode = "place"
-
-
-# connected through funciton in pikachu._ready()
-func _on_pikachu_clicked(object: CharacterBody2D):
-	selected_pikachus.append(object)
