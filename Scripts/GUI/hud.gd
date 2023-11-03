adb0c42aceebe4c2f80c35e3603b4af58c97d907
CdFerneding <bencf@stud.ntnu.no>
2023-11-03 09:44:03 +0100

    update weird godot naming with hud.gd

--- Scripts/GUI/hud.gd
+++ Scripts/GUI/hud.gd
@@ -1,11 +1,17 @@
 extends CanvasLayer
 
+var screensize = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Game.set_game_paused(true)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	screensize = get_viewport().size
+	$Background.set_size(screensize)
+
+
+func _on_start_game_pressed():
+	Game.set_game_paused(false)
+	self.queue_free()
