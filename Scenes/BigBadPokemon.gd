634f0a7a18fa6a664111174b12a32eb83e0c5f06
TxRuX <maharneu@gmail.com>
2023-11-15 10:21:33 +0100

    BigBadPokemon

--- Scenes/BigBadPokemon.gd
+++ Scenes/BigBadPokemon.gd
@@ -1,8 +1,31 @@
 extends BadPokemon
 
+var num_of_enemies = 3
+
+var rng = RandomNumberGenerator.new()
+
 func _ready():
-	pass
+	
+	super()
+	speed = 0
 	
 func _process(delta):
 	pass
 
+func _on_ennemy_spawner_timer_timeout():
+	
+	var mainPath = get_tree().get_root().get_node("Main")
+	var enemyPath = get_tree().get_root().get_node("Main/Enemies")
+
+	if enemyPath.get_child_count() >= num_of_enemies:
+		return
+	
+	var new_enemy = mainPath.enemy.instantiate()
+	new_enemy.position.x = 1591 + rng.randf_range(-20, 20)
+	new_enemy.position.y = 143 + rng.randf_range(-20, 20)
+	enemyPath.add_child(new_enemy)
+	# pikachus need the name Pikachu, to be recognized by resources farming-area
+	new_enemy.name = "Bad Pokachu"
+
+	# recall to have all pokachus from the "pokachus"-group in "pokachus"-variable again
+	mainPath.get_enemies()
