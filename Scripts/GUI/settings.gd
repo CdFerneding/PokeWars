8dac122ef02d4e3ebc7dae49593946cb1ff23ec6
CdFerneding <bencf@stud.ntnu.no>
2023-12-06 10:38:13 +0100

    fixed music and inconsistencies in intro dialogue music uses linear scale from 0 to 1 instead of decibel

--- Scripts/GUI/settings.gd
+++ Scripts/GUI/settings.gd
@@ -1,10 +1,9 @@
 extends CanvasLayer
 
-func _ready():
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 0)
+var music = AudioServer.get_bus_index("Master")
 
 func _on_h_slider_value_changed(value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
+	AudioServer.set_bus_volume_db(music , linear_to_db(value))
 
 
 func _input(event):
